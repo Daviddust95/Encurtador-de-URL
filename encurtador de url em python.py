e3792612930e3bb437c445d53d8ecf2cd6dc9590
Alisson David de Melo <daviddust4@hotmail.com>
2023-05-31 22:17:52 -0300

    Update encurtador de url em python.py
    Ajustes na forma de exibição no campo de digitar a URL.
    Agora o programa tem uma interface gráfica com comandos básicos.

--- encurtador de url em python.py
+++ encurtador de url em python.py
@@ -1,23 +1,39 @@
-import requests
-import json
+import tkinter as tk
+import pyshorteners as shr
+import tkinter.messagebox as messagebox
 
-access_token = "8e9d5422f15f9bcdd539d2e5db74f5cbec9780c0"
-url = "https://api-ssl.bitly.com/v4/shorten"
+def encurtar_url():
+    url_longa = entry.get()
+    shortener = shr.Shortener()
+    shortURL = shortener.tinyurl.short(url_longa)
+    label_resultado.config(text=f"A URL encurtada é: {shortURL}")
 
-headers = {
-    "Authorization": f"Bearer {access_token}",
-    "Content-Type": "application/json"
-}
+def copiar_url():
+    url_encurtada = label_resultado.cget("text")
+    janela.clipboard_clear()
+    janela.clipboard_append(url_encurtada.split(": ")[1])
+    messagebox.showinfo("Sucesso", "URL encurtada copiada para a área de transferência!")
 
-data = {
-    "long_url": "https://www.google.com/"
-}
+# Criação da janela
+janela = tk.Tk()
+janela.title("Encurtador de URL")
+janela.geometry("400x200")  # Definindo o tamanho da janela
 
-response = requests.post(url, headers=headers, data=json.dumps(data))
+# Criação dos widgets
+label_url = tk.Label(janela, text="Digite a URL:")
+label_url.pack()
 
-if response.status_code == 200:
-    response_data = json.loads(response.text)
-    short_url = response_data["link"]
-    print(short_url)
-else:
-    print("Erro ao encurtar URL")
+entry = tk.Entry(janela, width=50)  # Ajustando o tamanho do campo de entrada
+entry.pack()
+
+button_encurtar = tk.Button(janela, text="Encurtar", command=encurtar_url)
+button_encurtar.pack()
+
+label_resultado = tk.Label(janela, text="")
+label_resultado.pack()
+
+button_copiar = tk.Button(janela, text="Copiar", command=copiar_url)
+button_copiar.pack()
+
+# Execução da janela
+janela.mainloop()
